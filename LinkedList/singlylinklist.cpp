ab89682b56eee58f41b3cad18fc1dcb648e78954
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2023-06-20 11:58:14 +0530

    Started Learning Linked List

--- LinkedList/singlylinklist.cpp
+++ LinkedList/singlylinklist.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
+// single linked list - Navigation is forward
+/*
+Nodes - Data & Link
+        Data contains actual data
+        Link contains the address of next node of the list
+*/
+// double linked list - Navigation is forward as well as backward
+// circular linked list - Last element is linked to the first element
+// We can allocate Memory Dynamically.
+
 class Node{
     public:
-    int data;
-    Node* next;
+        int data;
+        Node* next;
 };
+
+void display(Node* head)
+{
+    while (head != NULL)
+    {
+        cout << head -> data;
+        head = head -> next;
+    }
+}
+
+int main()
+{
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head -> data = 1;
+    head -> next = second;
+    second -> data = 2;
+    second -> next = third;
+    third -> data = 3;
+    third -> next = NULL;
+
+    display(head);
+}
