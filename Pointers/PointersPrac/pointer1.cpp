e91862743b3e2be95dfb517c650266b01d21d696
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2023-06-26 11:43:40 +0530

    Revising Pointers

---
+++ Pointers/PointersPrac/pointer1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int* p;
+    a = 10;
+    p = &a; // address of a
+    cout << "a = " << a << endl;
+    *p = 12;  //dereferencing
+    cout << "a = " << a << endl;
+}
