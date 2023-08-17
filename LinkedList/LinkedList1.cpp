48821b0242a1b7ee4accf3f3e2a5f65e97f0cd5d
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2023-08-17 13:58:18 +0530

    Practicing....

---
+++ LinkedList/LinkedList1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    Node (int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList
+{
+    private:
+    Node* head;
+
+    public:
+    LinkedList()
+    {
+        this->head = nullptr;
+    }
+
+    void insertAtBeginning(int data)
+    {
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
+    
+    void printList()
+    {
+        Node* temp = head;
+        while (temp != nullptr)
+        {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main()
+{
+    LinkedList list;
+
+    list.insertAtBeginning(3);
+    list.insertAtBeginning(2);
+    list.insertAtBeginning(1);
+    list.printList();
+}
