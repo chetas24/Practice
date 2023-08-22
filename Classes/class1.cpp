8436a41b5c781e3c19141ebf760ced65c22054ea
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2023-08-22 13:17:13 +0530

    Practicing....

---
+++ Classes/class1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class C1
+{
+    int val{};
+    public:
+        void setValue(int value);
+        int getValue();
+};
+
+void C1::setValue(int value)
+    {
+        val = value;
+    }
+int C1::getValue()
+    {
+        return val;
+    }
+
+int main()
+{
+    C1 obj1;
+    obj1.setValue(3);
+    std::cout << "Value is ", obj1.getValue();
+}
