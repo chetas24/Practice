f93d8f3f3b0ec6cb0066525351e50be1db550d13
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-01-25 11:53:56 +0530

    Practicing....

--- LinkedList/LinkedList2.cpp
+++ LinkedList/LinkedList2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Node
 {
+public:
     int elem;
     class Node* next;
 };
@@ -11,34 +12,69 @@ class Node
 class LinkedList
 {
     class Node* head;
+public:
+    LinkedList();
+    ~LinkedList();
 
     void addFront(int x);
-    void removeFront(int x);
+    void removeFront();
     int len();
     int search(int x);
     void print();
+};
+
+LinkedList::LinkedList()
+{
+    head = NULL;
+}
+
+LinkedList::~LinkedList()
+{
+    while (head != NULL)
+    {
+        class Node *iter = head;
+        head = iter -> next;
+        delete iter;
+    }
 }
 
 void LinkedList::print()
 {
-
+    class Node* iter = head;
+    while (iter != NULL)
+    {
+        printf("%d", iter -> elem);
+        iter = iter -> next;
+    }
 }
 
 void LinkedList::addFront(int x)
 {
+    // Create a temporary pointer 'temp' to store the current head
     class Node* temp = head;  
+
+    // Create a new node and make it the new head
     head = new Node;   
+
+    // Set the element of the new head to the specified value 'x'
     head -> elem = x;   
-    head -> next = temp;   
+
+    // Update the next pointer of the new head to point to the previous head (temp)
+    head -> next = temp;     
 }
 
-void LinkedList::removeFront(int x)
+void LinkedList::removeFront()
 {
-    if (head == NULL) return;
+    // Check if the linked list is empty
+    if (head == NULL) 
+        return;
 
+    // Create a temporary pointer 'temp' to store the current head
     class Node* temp = head;
-    head = head -> next;
 
+    // Move the head to the next node, effectively removing the front node
+    head = head -> next;
+    delete temp;
 }
 
 int main()
@@ -48,5 +84,9 @@ int main()
     ll.addFront(30);
     ll.addFront(20);
     ll.addFront(10);
+
+    ll.print();
+    ll.removeFront();
+    ll.print();
 
 }
